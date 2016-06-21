ef55350ba7a1cc6b8263fcff7a21648b2115d573
alexanderlange53 <alex.lange_@gmx.de>
2016-06-21 14:25:12 +0200

    kleinere updates

--- Estimation_Stuttgart21_survey.R
+++ Estimation_Stuttgart21_survey.R
@@ -26,6 +26,8 @@ if(bearbeiter == 'Alex'){
 # Hilfsfunktionen
 source('./erster_test_functions.R')
 
+#--------------------------------# Schätzung mit stetiger räumlicher Variable #-------------------------------------------#
+
 # Da die Kategorie 'Keine Angabe' nicht in das Schema der geordneten Kategorien passt und keine Informationen
 # enthält wirde sie entfernt.
 for(i in 1:nrow(dataS)){
@@ -58,8 +60,9 @@ plot(model1, pages = 1)
 gam.check(model1)
 model1$family$getTheta(TRUE)
 
-
-#-------------# Schätzung mit transformierter response Variable #-------------#
+#-------------------------------------------------------------#
+# Schätzung mit transformierter response Variable (3 Klassen) #
+#-------------------------------------------------------------#
 
 # Zusammenführen von 'Sehr gut' und 'Gut' zu Zustimmung und 'Sehr Schlecht' und 'Schlecht' zu Ablehnend 
 for(i in 1:nrow(dataS)){
@@ -92,14 +95,23 @@ plot(model2, pages = 1)
 gam.check(model2)
 model2$family$getTheta(TRUE)
 
-#-------------# Schätzung mit diskreter räumlicher information #-----------------#
+#---------------------------------# Schätzung mit diskreter räumlicher information #---------------------------------------#
+
+#-----------------------------------------#
+# Bezirke als Informationen und 5 Klassen #
+#-----------------------------------------#
 
 bearbeiter = 'Alex'
 # loading data
 if(bearbeiter == 'Alex'){
+  dataS <- read.csv2('/home/alex/Schreibtisch/Uni/statistisches_praktikum/Auswertung/Neue_Daten/Stuttgart21_aufbereitet.csv',
+                     dec = '.')
   bezirke <- readOGR(dsn = "/home/alex/Schreibtisch/Uni/statistisches_praktikum/Auswertung/Geodaten/bezirke", layer = "bezirke")
 } else {
+  dataS <- read.csv2('/home/khusmann/mnt/U/Promotion/Kurse/Stat_Praktikum/Auswertung/Neue_Daten/Stuttgart21_aufbereitet.csv',
+                     dec = '.')
   bezirke <- readOGR(dsn = "/home/khusmann/mnt/U/Promotion/Kurse/Stat_Praktikum/Auswertung/Geodaten/bezirke/", layer = "bezirke")
+  
 }
 
 # Da die Kategorie 'Keine Angabe' nicht in das Schema der geordneten Kategorien passt und keine Informationen
@@ -110,7 +122,6 @@ for(i in 1:nrow(dataS)){
   }}
 dataS <- na.omit(dataS)
 
-# Schätzung mit 5 Kategorien und den Bezirken als diskrete räumliche Information
 ## gam parameter ##
 # Zielgroesse & Verteilungsannahme
 response <- "Meinung.zu.Stuttgart.21"
@@ -147,15 +158,9 @@ plot(model3, pages = 1)
 gam.check(model3)
 model3$family$getTheta(TRUE)
 
-# Schätzung mit 3 Kategorien und den Stadtbezirken als räumliche Information
-
-bearbeiter = 'Alex'
-# laden der Stadtteilinformationen
-if(bearbeiter == 'Alex'){
-  stadtteile <- readOGR(dsn = "/home/alex/Schreibtisch/Uni/statistisches_praktikum/Daten_Kneib/Stick/Daten_Kneib/Stadtteile_netto", layer = "Stadtteile_netto")
-} else {
-  stadtteile <- readOGR(dsn = "/home/khusmann/mnt/U/Promotion/Kurse/Stat_Praktikum/Auswertung/Geodaten/bezirke/", layer = "bezirke")
-}
+#-----------------------------------------#
+# Bezirke als Informationen und 3 Klassen #
+#-----------------------------------------#
 
 # Zusammenführen von 'Sehr gut' und 'Gut' zu Zustimmung und 'Sehr Schlecht' und 'Schlecht' zu Ablehnend 
 for(i in 1:nrow(dataS)){
@@ -174,7 +179,56 @@ for(i in 1:nrow(dataS)){
   if(dataS$Meinung.zu.Stuttgart.21[i] == 5){
     dataS$Meinung.zu.Stuttgart.21[i] <- 3
   }}
+
+
+## gam parameter ##
+# Zielgroesse & Verteilungsannahme
+response <- "Meinung.zu.Stuttgart.21"
 verteilung <- ocat(R=3)
+
+# Extrahieren der räumlichen Informationen der Stadtbezirke
+bezirke@data$id <- rownames(bezirke@data)
+helpdf <- fortify(bezirke, region = "id")
+bb <- merge(helpdf, bezirke@data, by = 'id', all.x = T)
+bb2 <- select(bb, long, lat, STADTBEZIR)
+bb3 <- list()
+for(i in levels(factor(bb2$STADTBEZIR))) {
+  bb3[[i]] <- bb2[bb2$STADTBEZIR == i, c('long', 'lat')]
+  
+}
+zt <- list(polys = bb3)
+
+# raeumlicher Effekt
+seff <- "s(Stadtbezirk, bs=\"mrf\", xt = zt)"
+
+# Parametrisch zu modellierende Kovariablen
+pars <- c("Familienstand", "Nationalität", "Geschlecht")
+
+# Potenziell nichtparametrisch zu modellierende Kovariablen
+nonpars <- c("Altersklasse.Befragter","Personenzahl.im.Haushalt","Monatliches.Netto.Haushaltseinkommen")
+
+# Erstellen der Schätzfunktion
+formel <- make.formula(response = response, fixed = seff, pars = pars, nonpars = nonpars)
+
+# GAM Schätzung
+model4 <- gam(formel, data = dataS, family= verteilung, method = 'REML')
+summary(model4)
+plot(model4, pages = 1)
+gam.check(model4)
+model4$family$getTheta(TRUE)
+
+
+#----------------------------------------------------------------------------#
+# Schätzung mit 3 Kategorien und den Stadtbezirken als räumliche Information #
+#----------------------------------------------------------------------------#
+
+bearbeiter = 'Alex'
+# laden der Stadtteilinformationen
+if(bearbeiter == 'Alex'){
+  stadtteile <- readOGR(dsn = "/home/alex/Schreibtisch/Uni/statistisches_praktikum/Daten_Kneib/Stick/Daten_Kneib/Stadtteile_netto", layer = "Stadtteile_netto")
+} else {
+  stadtteile <- readOGR(dsn = "/home/khusmann/mnt/U/Promotion/Kurse/Stat_Praktikum/Auswertung/Geodaten/bezirke/", layer = "bezirke")
+}
 
 # Extrahieren der räumlichen Informationen der Stadtbezirke
 stadtteile@data$id <- rownames(stadtteile@data)
