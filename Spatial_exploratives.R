50bea442cc549b6c2cc18e5de4c3a26d82d790a0
kaihusmann <kai.husmann@nw-fva.de>
2016-09-14 21:55:03 +0200

    tex: daten

--- Spatial_exploratives.R
+++ Spatial_exploratives.R
@@ -10,7 +10,7 @@ require(dplyr);require(gstat);require(raster)
 
 # loading data
 
-bearbeiter = 'Alex'
+bearbeiter = 'Kai@Home'
 
 if(bearbeiter == 'Alex'){
   dataS <- read.csv2('/home/alex/Schreibtisch/Uni/statistisches_praktikum/Auswertung/Neue_Daten/Stuttgart21_aufbereitet_stadtteile.csv',
@@ -33,6 +33,7 @@ if(bearbeiter == 'Kai@Work'){
 source('SpatialPlots.R')
 source('DataPrep.R')
 
+bezirke@data$STADTBEZIR
 # Plotten der Response Variablen mit Gauss-Krüger Informationen
 # Meinung Stuttgart 21
 GKPlot(dataS, bezirke = bezirke, Kategorien = 5)
