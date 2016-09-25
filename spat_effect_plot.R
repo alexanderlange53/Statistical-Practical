13c6dec3194f66543864981e9556b5d74a41809c
kaihusmann <kai.husmann@nw-fva.de>
2016-09-26 00:09:21 +0200

    spat plot

--- spat_effect_plot.R
+++ spat_effect_plot.R
@@ -1,4 +1,7 @@
 # Call Stuttgart21 ausführen bis Z. 158
+library(colorspace)
+colo <- diverge_hsv(3)
+
 
 summary(m1)
 str(m1$smooth[[1]]$S)
@@ -14,71 +17,30 @@ str(tt[[1]])
 
 matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x))
 
-persp(x = tt[[1]]$x, y = tt[[1]]$y, z = matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x)))
+#persp(x = tt[[1]]$x, y = tt[[1]]$y, z = matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x)), zlim = c(-0.1,0.1))
 
-spat <- data.frame(x = numeric() , y = numeric())
-for(i in c(1 : length(tt[[1]]$x))) {
-  
-  for(j in c(1 : length(tt[[1]]$y))) {
-    temp.df <- data.frame(x = tt[[1]]$x[i], y = tt[[1]]$y[j])
-    spat <- rbind(spat, temp.df)
-  }
-  
-}
+mat <- matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x))
+colnames(mat) <- tt[[1]]$x
+rownames(mat) <- tt[[1]]$y
 
+ggmat <- as.data.frame(melt(mat))
+names(ggmat) <- c("y", "x", "coef")
+
+# Idea 1: ggplot
+ggplot(ggmat, aes(x = x, y = y, fill = coef,  alpha = coef), show.legend = FALSE) + geom_tile() + 
+  scale_fill_gradient(low = colo[2],  high = "darkblue") +
+  theme_bw(13) + theme(axis.text = element_blank(),
+                       axis.ticks = element_blank()) +
+  scale_alpha(range = c(0.1,0.7), guide=FALSE) +
+  labs(x = NULL, y = NULL, fill = 'Koef.')
+
+
+
+# Idea 2_ With Contour
 spat <- cbind(spat, tt[[1]]$fit)
-names(spat)[3] <- "para"
 
-image(x = tt[[1]]$x, y = tt[[1]]$y, z = matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x)))
+par(mar = c(0, 1.5, 1.5, 0) + 0.5)
+image(x = tt[[1]]$x, y = tt[[1]]$y, z = matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x)), xlab = "", ylab = "", axes = F, col = colorRampPalette(brewer.pal(9, "Blues"))(500))
 contour(x = tt[[1]]$x, y = tt[[1]]$y, z = matrix(tt[[1]]$fit, nrow = length(tt[[1]]$x)), add = TRUE)
-
-# Convert Spat into a spatial point object
-spat.save <- spat
-
-# transform to spatial class
-spat$y <- as.numeric(as.character(spat$y)) # long
-spat$x <- as.numeric(as.character(spat$x)) # lat
-coordinates(spat) <- ~ y + x
-# assign CRS
-proj4string(spat) <- CRS("+init=epsg:31467")
-# reproject data
-spat <- spTransform(spat, CRS("+proj=longlat +datum=WGS84"))
-# loading map
-map <- get_map(location= rowMeans(bbox(ST21)), zoom=11, maptype = 'terrain', scale = 2)
-# transform back to data frame for ggplot
-spat.g <- as.data.frame(spat)
-# spatial transformation
-bezirke.t <- spTransform(bezirke, CRS("+proj=longlat"))
-
-
-  # plotting map
-  ggmap(map, extent = 'device', legend = 'bottom') + geom_polygon(data=bezirke.t, aes(x=long, y=lat, group=group),colour="black", alpha=0) +
-    stat_density2d(
-      aes(x = long, y = lat, fill = ..level..,  alpha = ..level..),
-      size = 2, bins = 8, data = data.f,
-      geom = "polygon"
-    ) + geom_density2d(data = data.f, 
-                       aes(x = long, y = lat, color = ..level..), size = 0.3, show.legend = F) +
-    theme_bw(13) + theme(axis.text = element_blank(), legend.position = 'bottom',
-                         axis.ticks = element_blank()) +
-    scale_fill_gradient(low=colo[2], high = 'darkblue')+
-    scale_color_gradient(low=colo[2], high = 'darkblue', guide = F)+
-    scale_alpha(range = c(0.1,0.7), guide=FALSE) +
-    labs(x = NULL, y = NULL, fill = 'Dichte') +
-    xlim(9.035, 9.32) + ylim(48.69, 48.87) +
-    facet_wrap(~ Response)
-  
-  # plotting map: spat. effect
-  ggmap(map, extent = 'device', legend = 'bottom') +
-    stat_density2d(
-      aes(x = x, y = y, fill = ..level..,  alpha = ..level..),
-      size = 2, bins = 8, data = spat.g,
-      geom = "polygon"
-    ) 
-    scale_fill_gradient(low=colo[2], high = 'darkblue') +
-    scale_color_gradient(low=colo[2], high = 'darkblue', guide = F) +
-   
-    scale_alpha(range = c(0.1,0.7), guide=FALSE) +
-    labs(x = NULL, y = NULL, fill = 'Dichte') +
-    xlim(9.035, 9.32) + ylim(48.69, 48.87)
-#}
+mtext(side = 3, line = 0.5, "Kontinuierlicher räumlicher Effekt")
+box()
