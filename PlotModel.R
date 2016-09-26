d555ce07dff762e87da3d4f192da2b353c8b87cc
alexanderlange53 <alex.lange_@gmx.de>
2016-09-26 21:50:43 +0200

    parameter effekte

--- PlotModel.R
+++ PlotModel.R
@@ -2,7 +2,7 @@ ggplot.model <- function(model, type="conditional", res=FALSE, variables, param 
                          col.line="black", col.point="darkblue", size.line=1, size.point=1) {
   require(visreg)
   require(plyr)
-  plotdata <- visreg(model, xvar = variables, type = type, plot = FALSE, scale = 'linear')
+  plotdata <- visreg(model, xvar = variables, type = type, plot = FALSE)
   smooths <- ldply(plotdata, function(part)   
     data.frame(Variable = part$meta$x, 
                x=part$fit[[part$meta$x]], 
